7d2f61a089cc722c70736fdddc096fc9e787ac23
unknown <mickel.kristen@gmail.com>
2022-11-19 19:09:55 -0500

    initial commit of Employee Tracker

---
+++ server.js
@@ -0,0 +1,56 @@
+//Previously, we were saving data in JSON files, but now we will be saving data in the database.
+//
+const fs = require('fs');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('console.table');
+
+//Here, I am creating the connection to my database.
+const db = mysql.createConnection(
+  {
+    //I am going to run this on my local machine.
+    host: 'localhost',
+    //Need my mysql username.
+    user: 'root',
+    //Need my mysql password.
+    password: 'UnitTest2022*!',
+    //Need my database name in order for my program to know which database to connect to.
+    database: ''
+  },
+  //This message lets me know that it connected successfully.
+  console.log('Connected to the employeeTracker database.')
+);
+
+//This is the list of options that the prompt will display to the user. 
+const initialQuestion = [
+  {
+    type: 'list',
+    name: 'options',
+    message: 'Please pick an option',
+    options: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+    //Need a filter function so that when the user selects an option, it will filter for that choice and return the value.
+    filter(val) {
+      return val.toLowerCase();
+    }
+  }
+]
+
+//Once the user selects an option, this function will run the appropriate query in the database.
+function runDbQuery() {
+  return inquirer.prompt(initialQuestion)
+  .then((selection) => {
+    if(selection === 'view all departments') {
+      //Provide the query you want to run and then a function.
+      db.query('SELECT * FROM department;', function (err, results) {
+        console.table(results);
+      });
+    } else {
+      //Add other selection options
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
+
+runDbQuery();
