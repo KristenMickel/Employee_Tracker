1e2177c12c147bbc1171b07e6ffd5891256f08fb
unknown <mickel.kristen@gmail.com>
2022-11-20 19:07:35 -0500

    Changed from if else logic to using a switch statement

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 //Previously, we were saving data in JSON files, but now we will be saving data in the database.
-//
-const fs = require('fs');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('console.table');
@@ -15,58 +13,88 @@ const db = mysql.createConnection(
     //Need my mysql password.
     password: 'UnitTest2022*!',
     //Need my database name in order for my program to know which database to connect to.
-    database: ''
-  },
-  //This message lets me know that it connected successfully.
-  console.log('Connected to the employeeTracker database.')
+    database: 'employeeTracker_db',
+  }
 );
 
-//This is the list of options that the prompt will display to the user. 
-const initialQuestion = [
-  {
-    type: 'list',
-    name: 'choices',
-    message: 'Please pick an option',
-    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
-    //Need a filter function so that when the user selects an option, it will filter for that choice and return the value.
-    filter(val) {
-      return val.toLowerCase();
-    }
-  }
-]
+db.connect((err) => {
+  if (err) throw err;
+  //This message lets me know that it connected successfully.
+  console.log('Connected to the employeeTracker database');
+  questionPrompt();
+});
 
-//Once the user selects an option, this function will run the appropriate query in the database.
-function runDbQuery() {
-  return inquirer.prompt(initialQuestion)
+
+//This is the list of options that the prompt will display to the user. 
+function questionPrompt() {
+  //Initial questions
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choices',
+      message: 'Please pick an option',
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+    },
+  ])
+  //Switch statement used to select a different function to execute based on the user's choice selection.
   .then((selection) => {
-    if(selection === 'view all departments') {
-      //Provide the query you want to run and then a function.
-      db.query('SELECT * FROM department;', function (err, results) {
-        console.table(results);
-      });
-    } else if (selection === 'view all roles') {
-        db.query('SELECT * FROM role;', function (err, results) {
-          console.table(results);
-        });
-    } else if (selection === 'view all employees') {
-        db.query('SELECT * FROM employee;', function (err, results) {
-          console.table(results);
-        });
-    } else if (selection === 'add a department') {
-      //Add other selection options
-    } else if (selection === 'add a role') {
-      //Add other selection options
-    } else if (selection === 'add as employee') {
-      //Add other selection options
-    } else if (selection === 'add an employee role'){
-      //Add other selection options
-    } else {
-      //"No selection was made"
+    console.log(selection.choices);
+    switch (selection.choices) {
+      case 'view all departments':
+        viewAllDepartments();
+        break;
+      case 'view all roles':
+        viewAllRoles();
+        break;
+      case 'view all employees':
+        viewAllEmployees();
+        break;
+      case 'add a department':
+        addDepartment();
+        break;
+      case 'add a role':
+        addRole();
+        break;
+      case 'add an employee':
+        addEmployee();
+        break;
+      case 'update an employee role':
+        updateEmployeeRole();
+        break;
+      default:
+        db.end();
     }
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  });
 }
 
-runDbQuery();
+//This query grabs everything from the department table
+function viewAllDepartments() {
+  db.query('SELECT * FROM department;', function (err, results) {
+    console.table(results);
+    questionPrompt();
+  });
+};
+
+//this query grabs everything from the role table
+function viewAllRoles() {
+  db.query('SELECT * FROM role;', function (err, results) {
+    console.table(results);
+    questionPrompt();
+  });
+};
+
+//This query grabs everything from the employees table
+function viewAllEmployees() {
+  db.query('SELECT * FROM employee;', function (err, results) {
+    console.table(results);
+    questionPrompt();
+  });
+};
+
+//Add a department
+
+//Add a role
+
+//Add an employee
+
+//Update an employee role
